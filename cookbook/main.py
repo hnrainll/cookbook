b315863b114a14cd4c4ab59d097ea2f60ad5820c
Hao <leo4chin@outlook.com>
2025-06-19 17:27:07 +0800

    移植飞书demo

--- cookbook/main.py
+++ cookbook/main.py
@@ -1,13 +1,41 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from loguru import logger
+
+from cookbook.feishu_bot import lark_ws_client
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await life_start()
+
+    yield
+
+    await life_end()
+
 
 # 创建 FastAPI 应用实例
 app = FastAPI(
-    title="Hello World API",
-    description="一个简单的 FastAPI Hello World 示例",
-    version="1.0.0"
+    title="Hello Cookbook",
+    description="一个简单的 FastAPI Hello Cookbook 示例",
+    version="1.0.0",
+    lifespan=lifespan
 )
+
+async def life_start():
+    logger.info("Start...")
+    await lark_ws_client.start()
+
+
+async def life_end():
+    logger.info("End...")
+
 
 @app.get("/")
 async def root():
     """根路径 - 最基础的 Hello World"""
     return {"message": "Hello Cookbook!"}
+
+
+import sys
+logger.info(sys.path)
